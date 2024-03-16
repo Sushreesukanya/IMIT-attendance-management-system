63821b850cc2997fb9239b8fc032d253f299ef8e
Sushree Sukanya Das <162134544+Sushreesukanya@users.noreply.github.com>
2024-03-17 00:11:15 +0530

    Update AttendanceTracker.java

--- AttendanceTracker.java
+++ AttendanceTracker.java
@@ -1,19 +1,24 @@
 package Attendance.Management.System;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
-import java.util.HashSet;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class AttendanceTracker extends JFrame {
+public class AttendanceTracker extends JFrame implements ActionListener {
 
     Choice choice1, choice2, choice3;
-    JTextField tfusername;
+    JTextField tfStartDate, tfEndDate;
     JButton view, back, update;
+    ResultSetMetaData rsmd;
+    DefaultTableModel model;
+    JScrollPane pane;
+    String[] col_name;
+    int rowCount;
 
     HashSet<String> values = new HashSet<>();
 
@@ -35,10 +40,10 @@ public class AttendanceTracker extends JFrame {
         panel1.setBounds(0, 50, 1600, 155);
         add(panel1);
 
-//        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("imgs/Imitlogo.png"));
-//        JLabel imagee = new JLabel(i1);
-//        imagee.setBounds(80, 3, 150, 150);
-//        panel1.add(imagee);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("imgs/Imitlogo.png"));
+        JLabel imagee = new JLabel(i1);
+        imagee.setBounds(80, 3, 150, 150);
+        panel1.add(imagee);
 
         // Heading part
         JLabel maintext2 = new JLabel("Attendance Tracker");
@@ -48,10 +53,10 @@ public class AttendanceTracker extends JFrame {
         panel1.add(maintext2);
 
         // IMIT logo
-//        ImageIcon i2 = new ImageIcon(ClassLoader.getSystemResource("imgs/Imitlogo.png"));
-//        JLabel image1 = new JLabel(i2);
-//        image1.setBounds(1280, 3, 150, 150);
-//        panel1.add(image1);
+        ImageIcon i2 = new ImageIcon(ClassLoader.getSystemResource("imgs/Imitlogo.png"));
+        JLabel image1 = new JLabel(i2);
+        image1.setBounds(1280, 3, 150, 150);
+        panel1.add(image1);
 
         // DropDown1
         JLabel choicename1 = new JLabel("Semester");
@@ -71,25 +76,10 @@ public class AttendanceTracker extends JFrame {
         choice1.setFont(new Font("Raleway", Font.BOLD, 13));
         add(choice1);
 
-        //DropDown2
-        JLabel choicename2 = new JLabel("Date");
-        choicename2.setBounds(270, 250, 100, 20);
-        choicename2.setBackground(new Color(29, 75, 100));
-        choicename2.setFont(new Font("Raleway", Font.BOLD, 15));
-        add(choicename2);
-
-        tfusername = new JTextField();
-        tfusername.setBounds(270, 273, 150, 20);
-        tfusername.setBorder(BorderFactory.createEmptyBorder());
-        tfusername.setBackground(new Color(29, 75, 100));
-        tfusername.setForeground(Color.WHITE);
-        tfusername.setFont(new Font("Raleway", Font.BOLD, 15));
-        tfusername.setCaretColor(Color.WHITE);
-        add(tfusername);
 
         //DropDown3
         JLabel choicename3 = new JLabel("Subject");
-        choicename3.setBounds(470, 250, 100, 20);
+        choicename3.setBounds(270, 250, 100, 20);
         choicename3.setBackground(new Color(29, 75, 100));
         choicename3.setFont(new Font("Raleway", Font.BOLD, 15));
         add(choicename3);
@@ -100,7 +90,7 @@ public class AttendanceTracker extends JFrame {
         choice2.add("OOAD");
         choice2.add("CN");
         choice2.add("IWP");
-        choice2.setBounds(470, 273, 150, 40);
+        choice2.setBounds(270, 273, 150, 20);
         choice2.setBackground(new Color(29, 75, 100));
         choice2.setForeground(Color.WHITE);
         choice2.setFont(new Font("Raleway", Font.BOLD, 13));
@@ -108,15 +98,47 @@ public class AttendanceTracker extends JFrame {
 
         //DropDown4
         JLabel choicename4 = new JLabel("Sections");
-        choicename4.setBounds(670, 250, 100, 20);
+        choicename4.setBounds(470, 250, 100, 20);
         choicename4.setBackground(new Color(29, 75, 100));
         choicename4.setFont(new Font("Raleway", Font.BOLD, 15));
         add(choicename4);
 
+        JLabel startDate = new JLabel("From");
+        startDate.setBounds(670, 250, 100, 20);
+        startDate.setBackground(new Color(29, 75, 100));
+        startDate.setFont(new Font("Raleway", Font.BOLD, 15));
+        add(startDate);
+
+
+        tfStartDate = new JTextField("YYYY-MM-DD");
+        tfStartDate.setBounds(670, 273, 150, 20);
+        tfStartDate.setBorder(BorderFactory.createEmptyBorder());
+        tfStartDate.setBackground(new Color(29, 75, 100));
+        tfStartDate.setForeground(Color.WHITE);
+        tfStartDate.setFont(new Font("Raleway", Font.BOLD, 15));
+        tfStartDate.setCaretColor(Color.WHITE);
+        add(tfStartDate);
+
+        JLabel endDate = new JLabel("To");
+        endDate.setBounds(870, 250, 100, 20);
+        endDate.setBackground(new Color(29, 75, 100));
+        endDate.setFont(new Font("Raleway", Font.BOLD, 15));
+        add(endDate);
+
+
+        tfEndDate = new JTextField("YYYY-MM-DD");
+        tfEndDate.setBounds(870, 273, 150, 20);
+        tfEndDate.setBorder(BorderFactory.createEmptyBorder());
+        tfEndDate.setBackground(new Color(29, 75, 100));
+        tfEndDate.setForeground(Color.WHITE);
+        tfEndDate.setFont(new Font("Raleway", Font.BOLD, 15));
+        tfEndDate.setCaretColor(Color.WHITE);
+        add(tfEndDate);
+
         choice3 = new Choice();
-        choice3.add("Sec A");
-        choice3.add("Sec B");
-        choice3.setBounds(670, 273, 150, 40);
+        choice3.add("A");
+        choice3.add("B");
+        choice3.setBounds(470, 273, 150, 40);
         choice3.setBackground(new Color(29, 75, 100));
         choice3.setForeground(Color.WHITE);
         choice3.setFont(new Font("Raleway", Font.BOLD, 13));
@@ -130,7 +152,7 @@ public class AttendanceTracker extends JFrame {
         back.setForeground(Color.WHITE);
         back.setFont(new Font("Raleway", Font.BOLD, 12));
         back.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        //back.addActionListener(this);
+        back.addActionListener(this);
         add(back);
 
         // view Button
@@ -141,12 +163,12 @@ public class AttendanceTracker extends JFrame {
         view.setForeground(Color.WHITE);
         view.setFont(new Font("Raleway", Font.BOLD, 12));
         view.setCursor(new Cursor(Cursor.HAND_CURSOR));
-       // view.addActionListener(this);
+        view.addActionListener(this);
         add(view);
 
 
         //Scrollable view of the table
-        JScrollPane pane = new JScrollPane();
+        pane = new JScrollPane();
         pane.setBounds(200, 350, 1300, 400);
         pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
@@ -159,86 +181,118 @@ public class AttendanceTracker extends JFrame {
         pane.getViewport().add(table);
 
 
-        //Fetching of the data from the data base
-        String query = "select * from mca_student_attendance_update where section = 'A'";
-        try {
-            Conn c = new Conn();
-            ResultSet rs = c.s.executeQuery(query);
-            ResultSetMetaData rsmd = rs.getMetaData();
-            DefaultTableModel model = (DefaultTableModel) table.getModel();
+        setVisible(true);
+    }
 
-            //Accessing column name directly from the table in the database
-            int cols = rsmd.getColumnCount();
-            String[] col_name = new String[cols];
-            for (int i = 0; i < cols; i++) {
-                col_name[i] = rsmd.getColumnName(i + 1);
-            }
-            model.setColumnIdentifiers(col_name);
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == back) {
+            setVisible(false);
+            new Dashboard();
+        } else if (ae.getSource() == view) {
+            String startdate=  tfStartDate.getText();
+            String enddate = tfEndDate.getText();
+            if (!isValidDateFormat(startdate)) {
+                JOptionPane.showMessageDialog(this, "Invalid date format. Please enter From date in YYYY-MM-DD format.");
+            } else if (!isValidDateFormat(enddate)) {
+                JOptionPane.showMessageDialog(this, "Invalid date format. Please enter To date in YYYY-MM-DD format.");
+            } else {
+
+                String sem = choice1.getSelectedItem();
+                String sub = choice2.getSelectedItem();
+                String sec = choice3.getSelectedItem();
+
+                //Fetching of the data from the database
+                String query = "select * from mca_student_attendance_update where section = '" + sec + "' and subject = '" + sub + "' and semester = '" + sem + "'";
+                try {
+                    Conn c = new Conn();
+                    ResultSet rs = c.s.executeQuery(query);
+                    rsmd = rs.getMetaData();
+                    model = (DefaultTableModel) table.getModel();
+
+                    //Accessing column name directly from the table in the database
+                    int cols = rsmd.getColumnCount();
+                    col_name = new String[cols];
+                    for (int i = 0; i < cols; i++) {
+                        col_name[i] = rsmd.getColumnName(i + 1);
+                    }
+                    model.setColumnIdentifiers(col_name);
 
 
-            //Accessing the data from the table
-            while (rs.next()) {
-                Object[] rowData = new Object[cols];
-                for (int i = 0; i < cols; i++) {
-                    rowData[i] = rs.getObject(i + 1);
-                }
-                model.addRow(rowData);
-            }
-
-            //Changing the Row values into columns
-            try{
-                String query1 = "SELECT DISTINCT updated_at FROM mca_student_attendance_update";
-                ResultSet rs1 = c.s.executeQuery(query1);
-
-                while(rs1.next()){
-                    String val = rs1.getString("updated_at");
-                    String columnName = val.toString();
-                    values.add(val);
-                    model.addColumn(columnName);
-
-                    //Assigning values to the temporary columns
-                        for(int i = 0; i < model.getRowCount(); i++){
-                            Object val1 = model.getValueAt(i, 5);
-                            model.setValueAt(val1, i, model.getColumnCount() - 1);
+                    //Accessing the data from the table
+                    while (rs.next()) {
+                        Object[] rowData = new Object[cols];
+                        for (int i = 0; i < cols; i++) {
+                            rowData[i] = rs.getObject(i + 1);
                         }
+                        model.addRow(rowData);
+                    }
+
+                    //Changing the Row values into columns
+                    try {
+//                        String query1 = "SELECT DISTINCT updated_at FROM mca_student_attendance_update";
+//                        ResultSet rs1 = c.s.executeQuery(query1);
+
+//                        while (rs1.next()) {
+//                            String val = rs1.getString("updated_at");
+//                            String columnName = val.toString();
+//                            values.add(val);
+                        // Create Calendar instances for start and end dates
+
+                        String[] startDateParts = startdate.split("-");
+                        int startYear = Integer.parseInt(startDateParts[0]);
+                        int startMonth = Integer.parseInt(startDateParts[1]);
+                        int startDay = Integer.parseInt(startDateParts[2]);
+
+                        Calendar startDate = Calendar.getInstance();
+                        startDate.set(startYear, startMonth - 1, startDay);
+
+                        String[] endDateParts = enddate.split("-");
+                        int endYear = Integer.parseInt(endDateParts[0]);
+                        int endMonth = Integer.parseInt(endDateParts[1]);
+                        int endDay = Integer.parseInt(endDateParts[2]);
+
+                        Calendar endDate = Calendar.getInstance();
+                        endDate.set(endYear,endMonth - 1, endDay);
+
+
+                        Calendar current = (Calendar) startDate.clone();
+
+                        while (current.before(endDate) || current.equals(endDate)) {
+                            int year = current.get(Calendar.YEAR);
+                            int month = current.get(Calendar.MONTH) + 1;
+                            int day = current.get(Calendar.DATE);
+                            String formattedMonth = String.format("%02d", month);
+                            String formattedDay = String.format("%02d", day);
+                            model.addColumn(year+ "-" + formattedMonth +"-"+ formattedDay);
+                            current.add(Calendar.DATE, 1);
+                        }
+
+                            //Assigning values to the temporary columns
+
+//                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    table.setModel(model);
+
+                    add(pane);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-
-            }catch(Exception e){
-                e.printStackTrace();
             }
-
-            table.setModel(model);
-
-                add(pane);
-
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-
-
-            setVisible(true);
         }
-//        public void actionPerformed (ActionEvent ae){
-//            if (ae.getSource() == back) {
-//                setVisible(false);
-//                //new Dashboard();
-//            } else if (ae.getSource() == update) {
-//                //Updation of the attendance
-//                for (int row : hs) {
-//                    Object changeValue = table.getValueAt(row, 3);
-//                    Conn c = new Conn();
-//                    String query = "UPDATE student SET attendance = '" + changeValue + "' WHERE regd_no = '220510200" + (row + 1) + "'";
-//                    try {
-//                        c.s.executeUpdate(query);
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//        }
-
-
+    }
     public static void main (String[]args){
         new AttendanceTracker();
+    }
+    // Method to validate the date format using regular expression
+    public static boolean isValidDateFormat(String dateString) {
+        String regex = "\\d{4}-(0?[1-9]|1[0-2])-\\d{2}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(dateString);
+        return matcher.matches();
     }
 }
